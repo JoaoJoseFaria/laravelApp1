e76e6d8ad25618deafcfcbf8b13857d0514fbe10
JoaoJoseFaria <jfaria.gmr@gmail.com>
2014-11-09 23:34:17 +0000

    integrar bootstrap

--- app/views/layout.blade.php
+++ app/views/layout.blade.php
@@ -3,13 +3,34 @@
     <head>
         <meta charset="UTF-8">
         <title>Learning Laravel Website </title>
+        <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
+        <link rel="stylesheet" href="{{ asset('css/style.css') }}">
     </head>
     <body>
-        <ul>
-            <li><a href="./">Home</a></li>
-            <li><a href="./about">About</a></li>
-            <li><a href="./contact">Contact</a></li>
-        </ul>
+        <header>
+            <nav class="navbar navbar-default" role="navigation">
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="/">Learning Laravel</a>
+                </div>
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    <ul class="nav navbar-nav navbar-right">
+                        <li><a href="/">Home</a></li>
+                        <li><a href="/about">About</a></li>
+                        <li><a href="/contact">Contact</a></li>
+                    </ul>
+                </div><!-- /.navbar-collapse -->
+            </nav>
+        </header>
         @yield('content')
+        <script src="https://code.jquery.com/jquery.js"></script>
+        <script language="JavaScript" src="{{ URL::asset('js/bootstrap.min.js') }}"></script>
     </body>
 </html>
